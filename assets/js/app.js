3b11965c39d614e6986745fc168887c9d56aae86
shivaydwivedi <polymath1510@gmail.com>
2025-02-19 03:33:16 +0530

    Added complete Hotel Management System project

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+// assets/js/app.js
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('availableRooms').innerText = 20;
+    document.getElementById('occupiedRooms').innerText = 10;
+    document.getElementById('bookingsToday').innerText = 5;
+});
